5d61f0c8c04f1e4edfde553bc01a70551188fc2a
Juboyme <84660284+Juboyme@users.noreply.github.com>
2021-08-11 23:23:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à !',
+    introDesc: `Trái đất vốn lạ thường,
+    mà sao em cứ đi nhầm đường,
+    lạc vào tim anh lẻ loi`,
+    btnIntro: 'Em cũng hông biết',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+   
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 😘',
+    messDesc: 'Chúc mừng sinh nhật em, cho thiếu qua dịch trả nhé',
+    btnAccept: 'Oke lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
